5cf57a0d32243bedcfc886a1be527f5b42815d79
JoeyBarrientes <josebarrientes3@gmail.com>
2024-05-19 11:55:30 -0500

    Complete JS TDD Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startInt,endInt) {
+    if (typeof startInt !== "number" || typeof endInt !== "number") {
+        return "ERROR";
+    }
+    if (startInt < 0 || endInt < 0) {
+        return "ERROR";
+    }
+    if (startInt > endInt) {
+        let temp = startInt;
+        startInt = endInt;
+        endInt = temp;
+    }
+    let sum = 0;
+    for (let i = startInt; i <= endInt; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
